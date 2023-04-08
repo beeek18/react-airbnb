13ff7783867197b94cb3ee49cf6afbd3fdd7a306
Bekzodbek <kodirov2000@list.ru>
2023-04-08 18:49:59 +0300

    Reservations screen (Loading guest reservations with server component)

---
+++ app/reservations/ReservationsClient.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import React, { useState, useCallback } from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+
+import { SafeReservation, SafeUser } from "../types";
+
+import Heading from "../components/Heading";
+import Container from "../components/Container";
+import ListingCard from "../components/listings/ListingCard";
+
+interface ReservationsClientProp {
+  reservations: SafeReservation[];
+  currentUser?: SafeUser | null;
+}
+
+const ReservationsClient: React.FC<ReservationsClientProp> = ({
+  reservations,
+  currentUser,
+}) => {
+  const router = useRouter();
+  const [deletingId, setDeletingId] = useState("");
+
+  const onCancel = useCallback(
+    (id: string) => {
+      setDeletingId(id);
+
+      axios
+        .delete(`/api/reservations/${id}`)
+        .then(() => {
+          toast.success("Reservation cancelled");
+          router.refresh();
+        })
+        .catch((error) => {
+          toast.error("Something went wrong");
+        })
+        .finally(() => {
+          setDeletingId("");
+        });
+    },
+    [router]
+  );
+
+  return (
+    <Container>
+      <Heading
+        title="Trips"
+        subtitle="Where you've been and where you're going"
+      />
+      <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+        {reservations.map((reservation) => (
+          <ListingCard
+            key={reservation.id}
+            data={reservation.listing}
+            reservation={reservation}
+            actionId={reservation.id}
+            onAction={onCancel}
+            disabled={deletingId === reservation.id}
+            actionLabel="Cancel guest reservation"
+            currentUser={currentUser}
+          />
+        ))}
+      </div>
+    </Container>
+  );
+};
+
+export default ReservationsClient;
