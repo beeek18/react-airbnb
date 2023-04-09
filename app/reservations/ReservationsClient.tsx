d118fb4fc9e852f5ff3e4a191a3f0960dcdca4c2
Bekzodbek <kodirov2000@list.ru>
2023-04-09 14:24:25 +0300

    Update ReservationsClient.tsx

--- app/reservations/ReservationsClient.tsx
+++ app/reservations/ReservationsClient.tsx
@@ -45,10 +45,7 @@ const ReservationsClient: React.FC<ReservationsClientProp> = ({
 
   return (
     <Container>
-      <Heading
-        title="Trips"
-        subtitle="Where you've been and where you're going"
-      />
+      <Heading title="Reservations" subtitle="Bookings on your properties" />
       <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
         {reservations.map((reservation) => (
           <ListingCard
